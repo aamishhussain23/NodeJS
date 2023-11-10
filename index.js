b366cf388dff6c469b0733fdcc1b7843c69e8f6f
Aamish Hussain <aamishhussain23@gmail.com>
2023-11-10 18:28:13 +0530

    learnt ejs basics

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express')
 
 const app = express()
-
 const port = 5000
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, resp) => {
-    resp.send('<h1>Home Page</h1>')
-})
-app.get('/about', (req, resp) => {
-    resp.send('<h1>About Page</h1>')
-})
-app.get('/contact', (req, resp) => {
-    resp.send('<h1>Contact Page</h1>')
-})
-app.get('*', (req, resp) => {
-    resp.send('<h1>404 ERROR</h1>')
+    resp.render('index', {name : 'AaMiSh'})
 })
 
 
