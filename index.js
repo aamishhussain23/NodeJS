dc09cac090148bb852650d0e5440d3499fd5d7d1
Aamish Hussain <aamishhussain23@gmail.com>
2023-11-11 00:05:56 +0530

    learnt ejs form data saving in array

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 
 const app = express()
-const port = 5000
+const port = 3000
+
+const db = []
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, resp) => {
-    resp.render('index', {name : 'AaMiSh'})
+    resp.render('index')
+})
+
+app.get('/success', (req, resp) => {
+    resp.render('success')
+})
+
+app.get('/data', (req, res) => {
+    res.send(db)
+})
+
+app.post('/success', (req, resp) => {
+    resp.redirect('/success')
+    console.log(req.body)
+    db.push({name : req.body.name, email : req.body.email})
 })
 
 
